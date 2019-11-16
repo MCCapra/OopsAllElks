52115198ee01454fe1e6b8ba5539ebf57438ee40
tps3871 <tps3871@rit.edu>
2019-11-16 17:24:09 -0500

    Script changes for music manager

---
+++ DoodleChemistry/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public static MusicManager instance;
+
+    [Header("Sound Effects")]
+    [SerializeField] private AudioSource sfxManager;
+    [SerializeField] private AudioClip[] sfxClips;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        // component version of a singleton
+        if (instance != null) return;
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+        GetComponent<AudioSource>().Play();
+    }
+
+    public void PlaySoundEffect(int index)
+    {
+        sfxManager.clip = sfxClips[index];
+        sfxManager.Play();
+    }
+}
