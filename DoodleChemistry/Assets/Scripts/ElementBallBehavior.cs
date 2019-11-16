0568d8eb9f39e092d392956d3aafc9740621f031
Michael Capra <michael.c.capra@gmail.com>
2019-11-16 11:06:19 -0500

    Shooter now cycles through list of elements to shoot

--- DoodleChemistry/Assets/Scripts/ElementBallBehavior.cs
+++ DoodleChemistry/Assets/Scripts/ElementBallBehavior.cs
@@ -7,15 +7,40 @@ public class ElementBallBehavior : MonoBehaviour
     public float magnitude;
     public Vector3 impulse; //This is going to be the up vector of the launcher
     public Element element;
+    private GameObject shooter;
+    [SerializeField] private GameObject text;
+    private string[] symbols = { "Na", "Cl", "C", "O", "H", "Fe" };
+    private int numBounces;
     // Start is called before the first frame update
     void Start()
     {
+        numBounces = 0;
+        shooter = GameObject.Find("Shooter");
         this.GetComponent<Rigidbody2D>().AddForce((impulse * magnitude), ForceMode2D.Impulse);
+        text.GetComponent<TextMesh>().text = symbols[(int)element];
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("Wall"))
+        {
+            numBounces++;
+            if(numBounces >= 4)
+            {
+                shooter.GetComponent<ShooterBehavior>().isFirable = true;
+                Destroy(this.gameObject);
+            }
+        }
+        else if(collision.transform.CompareTag("Death"))
+        {
+            shooter.GetComponent<ShooterBehavior>().isFirable = true;
+            Destroy(this.gameObject);
+        }
     }
 }
